f2722c88e7cdc9eef652d442444bbf46e2d99803
Benyamin <benjaminrabin@gmail.com>
2014-07-29 18:27:37 -0400

    using jekyll as a livereload server with guard

--- scripts.js
+++ scripts.js
@@ -2,6 +2,22 @@
 	var posuxApp = angular.module('posuxApp', ['ngRoute']);
 	var fbref= new Firebase("https://shining-fire-4822.firebaseio.com");
 	
+	
+		// create the controller and inject Angular's $scope
+	posuxApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Welcome to Joe\'s Cleaning!';
+	});
+
+	posuxApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	posuxApp.controller('contactController', function($scope) {
+		$scope.message = 'This is a contact page!';
+	});
+
+
 	// configure our routes
 	posuxApp.config(function($routeProvider) {
 		$routeProvider
@@ -23,19 +39,5 @@
 				templateUrl : 'pages/contact.html',
 				controller  : 'contactController'
 			});
-	});
-
-	// create the controller and inject Angular's $scope
-	posuxApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Welcome to Joe\'s Cleaning!';
-	});
-
-	posuxApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
-	});
-
-	posuxApp.controller('contactController', function($scope) {
-		$scope.message = 'This is a contact';
 	});
 

1f9bba5e6a3d339c28b861768d91f7ecd9beb09a
Benyamin <benjaminrabin@gmail.com>
2014-07-30 06:32:55 -0400

    web dev moves so fast :/

---
+++ js/services.js
@@ -0,0 +1,12 @@
+(function() {
+   'use strict';
+
+   /* Services */
+
+   angular.module('joe.services', ['joe.service.login', 'joe.service.firebase', 'joe.service.changeEmail'])
+
+      // put your services here!
+      // .service('serviceName', ['dependency', function(dependency) {}]);
+
+})();
+
